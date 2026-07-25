71a742943d3f146592226e4352d5d9bfeac0ed0e
Shahzain Shafique <88544736+shahzainshafique@users.noreply.github.com>
2026-07-26 00:09:41 +0500

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+const n = nums.length;
+
+    const left = new Array(n);
+    const right = new Array(n);
+    const output = new Array(n);
+
+    left[0] = 1;
+    for (let i = 1; i < n; i++) {
+        left[i] = left[i - 1] * nums[i - 1];
+    }
+
+    right[n - 1] = 1;
+    for (let i = n - 2; i >= 0; i--) {
+        right[i] = right[i + 1] * nums[i + 1];
+    }
+
+    for (let i = 0; i < n; i++) {
+        output[i] = left[i] * right[i];
+    }
+
+    return output;
+    }
+}
