1519827c51bce201043384445c1a50bbf5e826ab
Shahzain Shafique <88544736+shahzainshafique@users.noreply.github.com>
2026-07-26 00:04:34 +0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for (const word of strs){
+
+            const key = word.split("").sort().join("");
+
+            if (!map.has(key)){
+                map.set(key,[]);
+            }
+
+            map.get(key).push(word);
+        }
+return Array.from(map.values())
+
+    }
+}
