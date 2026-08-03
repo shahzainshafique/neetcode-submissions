8979de11bf66c134137258535dc74f692ede0320
Shahzain Shafique <88544736+shahzainshafique@users.noreply.github.com>
2026-08-03 16:24:42 +0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,31 @@
+class MinStack {
+constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+
+        if (this.minStack.length === 0) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(
+                Math.min(val, this.minStack[this.minStack.length - 1])
+            );
+        }
+    }
+
+    pop() {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
